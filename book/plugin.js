f400baea8be5c16b6fcde82a4b717b8abedd323f
Johan Preynat <johan.preynat@gmail.com>
2016-04-21 10:11:33 +0200

    Cleanup front-end code

--- book/plugin.js
+++ book/plugin.js
@@ -2,8 +2,4 @@ require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         config.site_id = config.site_id || {};
     });
-
-    gitbook.events.bind("exercise.submit", function(e, data) {
-
-    });
 });
